ef622172b92b1fa7feb6978f20a989d2ff2beb8d
sohaghareb <soha.ghareb@student.guc.edu.eg>
2015-03-19 09:19:37 +0200

    Communicate with the website's database via Http requests #12

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: :json } do
+      resources :records do
+        collection do
+          post 'get_records_by_email'
+          post 'save_record'
+          get 'get_level'
+        end
+      end
+
+    end
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
