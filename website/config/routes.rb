9a2254b73fb2f69720bf1b4d54bffe1dfd87e15b
Mariam Mohamed Fawzy <maryammohamed61@gmail.com>
2015-03-20 18:29:10 +0200

    game view #47

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
           post 'get_records_by_email'
           post 'save_record'
           get 'get_level'
+          post 'user_login'
         end
       end
   end
